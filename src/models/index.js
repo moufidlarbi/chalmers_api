e57241ef2b2cff6100215a418463f3e45d0da44a
Moufid Larbi <moufidlarbi@gmail.com>
2019-10-21 23:28:21 -0400

    fixed error

--- src/models/index.js
+++ src/models/index.js
@@ -1,29 +1,5 @@
-// const shelters = {
-//   1: {
-//     id: '1',
-//     address: '123 test address'
-//   },
-//   2: {
-//     id: '2',
-//     address: '99 test address'
-//   }
-// }
-
-// const meals = {
-//   1: {
-//     id: '1',
-//     address: '123 test address',
-//     date: 123456789
-//   },
-//   2: {
-//     id: '2',
-//     address: '123 test address',
-//     date: 123456789
-//   }
-// }
-
-import shelters from '../data/shelters.json'
-import meals from '../data/meals.json'
+import shelters from '../data/toronto/shelters.json'
+import meals from '../data/toronto/meals.json'
 
 export default {
   shelters,
