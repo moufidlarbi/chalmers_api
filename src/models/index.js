c953c37de8fff978e140175702f2c5f2573df78f
Moufid Larbi <moufidlarbi@gmail.com>
2019-10-21 23:15:39 -0400

    moved data to separate json files

--- src/models/index.js
+++ src/models/index.js
@@ -1,26 +1,29 @@
-const shelters = {
-  1: {
-    id: '1',
-    address: '123 test address'
-  },
-  2: {
-    id: '2',
-    address: '99 test address'
-  }
-}
+// const shelters = {
+//   1: {
+//     id: '1',
+//     address: '123 test address'
+//   },
+//   2: {
+//     id: '2',
+//     address: '99 test address'
+//   }
+// }
 
-const meals = {
-  1: {
-    id: '1',
-    address: '123 test address',
-    date: 123456789
-  },
-  2: {
-    id: '2',
-    address: '123 test address',
-    date: 123456789
-  }
-}
+// const meals = {
+//   1: {
+//     id: '1',
+//     address: '123 test address',
+//     date: 123456789
+//   },
+//   2: {
+//     id: '2',
+//     address: '123 test address',
+//     date: 123456789
+//   }
+// }
+
+import shelters from '../data/shelters.json'
+import meals from '../data/meals.json'
 
 export default {
   shelters,
