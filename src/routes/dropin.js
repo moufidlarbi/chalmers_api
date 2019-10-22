028edeed1b043ca43480f45362a26b1620fd1ad9
Moufid Larbi <moufidlarbi@gmail.com>
2019-10-22 01:34:00 -0400

    moved dropins to mock api

--- src/routes/dropin.js
+++ src/routes/dropin.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
+import fetch from 'node-fetch'
+
 const router = Router()
 
-router.get('/', (req, res) => {
-  return res.send(Object.values(req.context.models.meals))
-})
-
-router.get('/:mealId', (req, res) => {
-  return res.send(req.context.models.meals[req.params.mealId])
+router.get('/', function (req, res, next) {
+  fetch('http://5dae93e7c7e88c0014aa34b7.mockapi.io/dropins')
+    .then(response => response.json())
+    .then(json => res.send(json))
 })
 
 export default router
