436a952af601e3359a97d94339f1c48c1e4284cb
Moufid Larbi <moufidlarbi@gmail.com>
2019-10-21 21:47:58 -0400

    added express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Success!');
+  });
+  
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`),
+);
